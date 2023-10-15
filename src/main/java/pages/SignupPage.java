1cad22b8083c2dfd998c25f935c98f38c610a297
Ivana Ivkov <ivkov.ivana9@gmail.com>
2023-10-15 23:05:38 +0200

    Add selectors for sign up page

--- src/main/java/pages/SignupPage.java
+++ src/main/java/pages/SignupPage.java
@@ -1,4 +1,33 @@
 package pages;
 
-public class SignupPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SignupPage extends BasicPage {
+    public SignupPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public WebElement getFieldForNameInput() {
+        return driver.findElement(By.id("name"));
+
+    }
+
+    public WebElement getFieldForEmailInput() {
+        return driver.findElement(By.id("email"));
+    }
+
+    public WebElement getFieldForPasswordInput() {
+        return driver.findElement(By.id("password"));
+    }
+
+    public WebElement getFieldForConfirmPassword() {
+        return driver.findElement(By.id("confirmPassword"));
+    }
+
+    public WebElement getSignMeButton() {
+        return driver.findElement(By.cssSelector("button.v-btn--is-elevated.secondary"));
+    }
 }
