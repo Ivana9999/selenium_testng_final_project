0b768c2a684514d9c91267dd28e16804d20ace14
Ivana Ivkov <ivkov.ivana9@gmail.com>
2023-10-15 22:54:49 +0200

    Add selectors for nav page

--- src/main/java/pages/NavPage.java
+++ src/main/java/pages/NavPage.java
@@ -1,4 +1,60 @@
 package pages;
 
-public class NavPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NavPage extends BasicPage {
+    public NavPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public WebElement getHomeLink() {
+        return driver.findElement(By.cssSelector("a[href='/']"));
+    }
+
+    public WebElement getAboutLink() {
+        return driver.findElement(By.cssSelector("a.btnAbout[href='/about']"));
+    }
+
+    public WebElement getMyProfileLink() {
+        return driver.findElement(By.cssSelector("a.btnProfile[href='/profile']"));
+    }
+
+    public WebElement getAdminButton() {
+        return driver.findElement(By.cssSelector("button.btnAdmin"));
+    }
+
+    public WebElement getCitiesLinkFromAdminDropDownMenu() {
+        return driver.findElement(By.cssSelector("a.btnAdminCities[href='/admin/cities']"));
+    }
+
+    public WebElement getUsersLinkFromAdminDropDownMenu() {
+        return driver.findElement(By.cssSelector("a.btnAdminUsers[href='/admin/users']"));
+    }
+
+    public WebElement getSignUpButton() {
+        return driver.findElement(By.cssSelector("a.btnLogin[href='/signup']"));
+    }
+
+    public WebElement getLoginButton() {
+        return driver.findElement(By.cssSelector("a.btnLogin[href='/login']"));
+    }
+    public WebElement getLogoutButton() {
+        return driver.findElement(By.cssSelector("button.btnLogout"));
+    }
+    public WebElement getENButtonFromLanguageDropDownMenu() {
+        return driver.findElement(By.cssSelector("div.btnEN"));
+    }
+    public WebElement getESButtonFromLanguageDropDownMenu() {
+        return driver.findElement(By.cssSelector("div.btnES"));
+    }
+    public WebElement getFRButtonFromLanguageDropDownMenu() {
+        return driver.findElement(By.cssSelector("div.btnFR"));
+    }
+    public WebElement getCNButtonFromLanguageDropDownMenu() {
+        return driver.findElement(By.cssSelector("div.btnCN"));
+    }
+
 }
