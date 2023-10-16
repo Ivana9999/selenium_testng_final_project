8cddb74084103ae4d4ae03f60d21bacf824e91c3
Ivana Ivkov <ivkov.ivana9@gmail.com>
2023-10-16 21:22:26 +0200

    Add get popup and get text inside popup methods

--- src/main/java/pages/MessagePopUpPage.java
+++ src/main/java/pages/MessagePopUpPage.java
@@ -1,4 +1,29 @@
 package pages;
 
-public class MessagePopUpPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class MessagePopUpPage extends BasicPage{
+    public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+    public void waitPopUptoBeVisible () {
+        wait
+                .withMessage("Pop up should be visible")
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.v-snack__content")));
+    }
+    public WebElement getPopUp () {
+        return driver.findElement(By.cssSelector("div.v-snack__content"));
+    }
+
+    public String getTextInsidePopUp () {
+      List <WebElement> popUp = getPopUp().findElement(By.tagName("ul")).findElements(By.tagName("li"));
+     return popUp.get(0).getText();
+    }
+
 }
