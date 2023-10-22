60c74b0447f2d1203b15ea31b4cb363c86c7d9db
Ivana Ivkov <ivkov.ivana9@gmail.com>
2023-10-22 18:50:14 +0200

    Add auth routes test class

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -1,2 +1,29 @@
-package tests;public class AuthRoutesTests {
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BasicTest{
+
+    @Test
+    public void forbidsVisitsToHomeUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + "/home");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Current url should be " + baseUrl + "/login");
+
+    }
+    @Test
+    public void ForbidsVisitsToProfileUrlIfNotAuthenticated () {
+         driver.navigate().to(baseUrl + "/profile");
+         Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Current url should be " + baseUrl + "/login");
+    }
+    @Test
+    public void ForbidsVisitsToAdminCitiesUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + "/admin/cities");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Current url should be " + baseUrl + "/login");
+    }
+    @Test
+    public void  ForbidsVisitsToAdminUsersUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + "/admin/users");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Current url should be " + baseUrl + "/login");
+    }
 }
