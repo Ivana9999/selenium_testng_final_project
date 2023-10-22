abcce5c94a353bd2e9dbadeb4f3242c7f2479a2e
Ivana Ivkov <ivkov.ivana9@gmail.com>
2023-10-22 18:50:52 +0200

    Add tests for changing the language

--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -1,2 +1,36 @@
-package tests;public class LocaleTests {
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LocaleTests extends BasicTest{
+
+    @Test
+    public void setLocaleToES () {
+        navPage.clickOnLanguageButton();
+        navPage.getESButtonFromLanguageDropDownMenu().click();
+        Assert.assertEquals(navPage.getHeader().getText(), "Página de aterrizaje");
+    }
+
+    @Test
+    public void setLocaleToEN () {
+        navPage.clickOnLanguageButton();
+        navPage.getENButtonFromLanguageDropDownMenu().click();
+        Assert.assertEquals(navPage.getHeader().getText(), "Landing");
+    }
+
+    @Test
+    public void setLocaleToCN () {
+        navPage.clickOnLanguageButton();
+        navPage.getCNButtonFromLanguageDropDownMenu().click();
+        Assert.assertEquals(navPage.getHeader().getText(), "首页");
+    }
+
+
+    @Test
+    public void setLocaleToFR () {
+        navPage.clickOnLanguageButton();
+        navPage.getFRButtonFromLanguageDropDownMenu().click();
+        Assert.assertEquals(navPage.getHeader().getText(), "Page d'atterrissage");
+    }
 }
